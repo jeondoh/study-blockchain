a251d0e816e307c338d90183ec1f1a4ef8ab389a
jeondoh <jundo1302@gmail.com>
2022-10-13 09:53:47 +0900

    reduce allowance, 함수 리팩토링

--- simple-wallet/contract/SimpleWallet.sol
+++ simple-wallet/contract/SimpleWallet.sol
@@ -4,9 +4,27 @@ pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-contract SimpleWallet is Ownable {
+contract Allowance is Ownable {
+    mapping(address => uint) public allowance;
+    
+    modifier onwerOrAllowed(uint _amount) {
+        require(allowance[msg.sender] >= _amount, "You are not allowed");
+        _;
+    }
 
-    function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
+    function addAllowance(address _who, uint _amount) public onlyOwner {
+        allowance[_who] = _amount;
+    }
+
+    function reduceAllowance(address _who, uint _amount) internal {
+        allowance[_who] -= _amount;
+    }
+}
+
+contract SimpleWallet is Allowance {
+    function withdrawMoney(address payable _to, uint _amount) public onwerOrAllowed(_amount) {
+        require(_amount <= address(this).balance, "there are not enough funds stored in the smart contract");
+        reduceAllowance(msg.sender, _amount);
         _to.transfer(_amount);
     }
 
