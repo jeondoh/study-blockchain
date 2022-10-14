33f7d0d6c74e6e93c89ae1f3100cb4048b935641
jeondoh <jundo1302@gmail.com>
2022-10-14 12:57:16 +0900

    mint contract 작성

--- basic-minting-animal/contract/MintAnimalToken.sol
+++ basic-minting-animal/contract/MintAnimalToken.sol
@@ -3,8 +3,17 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract MintAnimalToken {
-    function MintAnimalToken(){
+contract MintAnimalToken is ERC721Enumerable {
+    constructor() ERC721("h662Animals", "HAS") {}
 
+    mapping(uint256 => uint256) public animalTypes;
+
+    function mintAnimalToken() public {
+        // totalSupply : 현재까지 민팅된 NFT 총량
+        uint256 animalTokenId = totalSupply() + 1;
+        uint256 animalType = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, animalTokenId))) % 5 + 1;
+        animalTypes[animalTokenId] = animalType;
+
+        _mint(msg.sender, animalTokenId);
     }
 }
