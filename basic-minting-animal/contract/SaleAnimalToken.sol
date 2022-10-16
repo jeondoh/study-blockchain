b7dd865835edb2c8fbc2845b86d68785b202e040
jeondoh <jundo1302@gmail.com>
2022-10-16 22:47:02 +0900

    민팅된 NFT 구매 contract 작성

--- basic-minting-animal/contract/SaleAnimalToken.sol
+++ basic-minting-animal/contract/SaleAnimalToken.sol
@@ -14,6 +14,7 @@ contract SaleAnimalToken {
     // 현재 판매중인 animal array, FE와 연동
     uint256[] public onSaleAnimalTokenArray;
 
+    // NFT 를 판매하는 함수
     function setForSaleAnimalToken(uint256 _animalTokenId, uint256 _price) public {
         // ownerOf : 지정된 토큰 ID의 소유자를 가져옴
         address animalTokenOwner = mintAnimalTokenAddress.ownerOf(_animalTokenId);
@@ -26,5 +27,32 @@ contract SaleAnimalToken {
 
         animalTokenPrices[_animalTokenId] = _price;
         onSaleAnimalTokenArray.push(_animalTokenId);
+    }
+
+    // NFT 를 구매하는 함수
+    function purchaseAnimalToken(uint256 _animalTokenId) public payable {
+        uint256 price = animalTokenPrices[_animalTokenId];
+        address animalTokenOwner = mintAnimalTokenAddress.ownerOf(_animalTokenId);
+        
+        require(price > 0, "Animal token not sale");
+        require(price <= msg.value, "Caller sent lower than price");
+        require(animalTokenOwner != msg.sender, "Caller is animal token owner");
+        // animalToken의 소유자에게 돈을 지불
+        payable(animalTokenOwner).transfer(msg.value);
+        // 구매자에게 구매한 NFT를 보냄
+        // 인자 (보내는사람, 받는사람, 보낼내용)
+        mintAnimalTokenAddress.safeTransferFrom(animalTokenOwner, msg.sender, _animalTokenId);
+        // 초기화
+        animalTokenPrices[_animalTokenId] = 0;
+        for(uint256 i = 0; i < onSaleAnimalTokenArray.length; i++) {
+            if(animalTokenPrices[onSaleAnimalTokenArray[i]] == 0) {
+                onSaleAnimalTokenArray[i] = onSaleAnimalTokenArray[onSaleAnimalTokenArray.length - 1];
+                onSaleAnimalTokenArray.pop();
+            }
+        }
+    }
+
+    function getOnSaleAnimalTokenArrayLength() view public returns (uint256) {
+        return onSaleAnimalTokenArray.length;
     }
 }
