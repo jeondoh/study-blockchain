34c91b741305d0f4b862ebd2be75fe8c3bb67f96
jeondoh <jundo1302@gmail.com>
2022-10-17 15:27:02 +0900

    feat: minting 된 항목 목록

--- basic-minting-animal/frontend/src/routes/myAnimal.tsx
+++ basic-minting-animal/frontend/src/routes/myAnimal.tsx
@@ -1,11 +1,57 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
+import { mintAnimalTokenContract } from "../contracts";
+import animalCard from "../components/animalCard";
+import { Grid } from "@chakra-ui/react";
+import AnimalCard from "../components/animalCard";
 
 interface MyAnimalProps {
   account: string;
 }
 
 const MyAnimal = ({ account }: MyAnimalProps) => {
-  return <div>MA</div>;
+  const [animalCardArray, setAnimalCardArray] = useState<string[]>();
+  const getAnimalTokens = async () => {
+    try {
+      const balanceLength = await mintAnimalTokenContract.methods
+        .balanceOf(account)
+        .call();
+
+      const tempAnimalCardArray = [];
+
+      for (let i = 0; i < parseInt(balanceLength, 10); i++) {
+        const animalTokenId = await mintAnimalTokenContract.methods
+          .tokenOfOwnerByIndex(account, i)
+          .call();
+
+        const animalType = await mintAnimalTokenContract.methods
+          .animalTypes(animalTokenId)
+          .call();
+        tempAnimalCardArray.push(animalType);
+      }
+      setAnimalCardArray(tempAnimalCardArray);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    if (!account) return;
+
+    getAnimalTokens();
+  }, [account]);
+
+  useEffect(() => {
+    console.log(animalCardArray);
+  }, [animalCardArray]);
+
+  return (
+    <Grid templateColumns="repeat(4, 1fr)" gap={8}>
+      {animalCardArray &&
+        animalCardArray.map((v, i) => {
+          return <AnimalCard key={i} animalType={v} />;
+        })}
+    </Grid>
+  );
 };
 
 export default MyAnimal;
