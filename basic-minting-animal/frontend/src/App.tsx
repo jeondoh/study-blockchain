d69d83b3d5f64a177112e458c71746470fa65f41
jeondoh <jundo1302@gmail.com>
2022-10-17 13:50:21 +0900

    feat: web3를 이용한 메타마스트, 컨트렉트 초기 연결

--- basic-minting-animal/frontend/src/App.tsx
+++ basic-minting-animal/frontend/src/App.tsx
@@ -1,7 +1,35 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./routes/main";
 
 const App: FC = () => {
-  return <div>web3-boilerplate</div>;
+  const [account, setAccount] = useState<String>("");
+  const getAccount = async () => {
+    try {
+      if (window.ethereum) {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setAccount(accounts[0]);
+      } else {
+        alert("install metamask");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getAccount();
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ basic-minting-animal/frontend/src/routes/main.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+
+const Main = () => {
+  return <Box>Main</Box>;
+};
+
+export default Main;
