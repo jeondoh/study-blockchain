878317cd0d622d4fec115c775dd756d4254d7e20
jeondoh <jundo1302@gmail.com>
2022-10-27 23:57:06 +0900

    feat: calculate time until function finished

--- hardhat-begin/scripts/deploy.js
+++ hardhat-begin/scripts/deploy.js
@@ -1,5 +1,4 @@
 const { ethers } = require("hardhat");
-const { rpcLog } = require("hardhat/internal/core/jsonrpc/types/output/log");
 
 async function main() {
   const [deployer] = await ethers.getSigners();
@@ -12,21 +11,30 @@ async function main() {
   );
 
   const Token = await ethers.getContractFactory("Token");
+  const startTime = new Date();
   const contract = await Token.deploy().catch((error) => console.log(error));
   const { chainId, hash, gasPrice, gasLimit, nonce } =
     contract.deployTransaction;
+  await contract.deployed();
+  const endTime = new Date();
+  const resultTime = endTime - startTime;
+  const resultSecond = (resultTime / 1000) % 60;
+  console.log(resultSecond);
+
+  if (resultSecond >= 30) {
+    console.log("시간초과 함수종료");
+    process.exit(0);
+  }
 
   console.log(`Token Address : ${contract.address}`);
   console.log(`chainId : ${chainId}`);
   console.log(`nonce : ${nonce}`);
   console.log(`hash : ${hash}`);
-  console.log(`gasPrice : ${gasPrice}`);
+  console.log(`gasPrice : ${ethers.utils.formatEther(gasPrice)}`);
   console.log(`gasLimit : ${gasLimit}`);
   console.log(
     `gas Total(eth) : ${ethers.utils.formatEther(gasPrice.mul(gasLimit))}`
   );
-
-  await contract.deployed();
 }
 
 main()
