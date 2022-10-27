34970283e66caea64b9e484787d69b7f5392c54f
jeondoh <jundo1302@gmail.com>
2022-10-27 23:43:15 +0900

    feat: set deploy contract, logged deployTransaction

--- hardhat-begin/scripts/deploy.js
+++ hardhat-begin/scripts/deploy.js
@@ -1,16 +1,32 @@
 const { ethers } = require("hardhat");
+const { rpcLog } = require("hardhat/internal/core/jsonrpc/types/output/log");
 
 async function main() {
   const [deployer] = await ethers.getSigners();
 
-  console.log("Deploying contracts with the account:", deployer.address);
-
-  console.log("Account balance:", (await deployer.getBalance()).toString());
+  console.log(`Deploying contracts with the account: ${deployer.address}`);
+  console.log(
+    `Account balance(eth): ${ethers.utils.formatEther(
+      (await deployer.getBalance()).toBigInt()
+    )}`
+  );
 
   const Token = await ethers.getContractFactory("Token");
-  const token = await Token.deploy();
+  const contract = await Token.deploy().catch((error) => console.log(error));
+  const { chainId, hash, gasPrice, gasLimit, nonce } =
+    contract.deployTransaction;
 
-  console.log("Token address:", token.address);
+  console.log(`Token Address : ${contract.address}`);
+  console.log(`chainId : ${chainId}`);
+  console.log(`nonce : ${nonce}`);
+  console.log(`hash : ${hash}`);
+  console.log(`gasPrice : ${gasPrice}`);
+  console.log(`gasLimit : ${gasLimit}`);
+  console.log(
+    `gas Total(eth) : ${ethers.utils.formatEther(gasPrice.mul(gasLimit))}`
+  );
+
+  await contract.deployed();
 }
 
 main()
